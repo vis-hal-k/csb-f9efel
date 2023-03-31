c05fdcd6be86a3de3c4471ec15518cb026653c1c
Code Sandbox <hello@codesandbox.io>
2023-03-31 14:06:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.df292be9ae7c7dc0456daa3288f4c595.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f7b0cb4bf83f9e841a000b8918495a",
+    "url": "https://vis-hal-k.github.io/csb-f9efel/index.html"
+  },
+  {
+    "revision": "aad9d73243c79cd31361",
+    "url": "https://vis-hal-k.github.io/csb-f9efel/static/js/2.6e1939c1.chunk.js"
+  },
+  {
+    "revision": "4826cc833777820f9148",
+    "url": "https://vis-hal-k.github.io/csb-f9efel/static/js/main.e0cc9441.chunk.js"
+  },
+  {
+    "revision": "d5ca551fb693d03a0cbb",
+    "url": "https://vis-hal-k.github.io/csb-f9efel/static/js/runtime-main.67acf87c.js"
+  }
+]);
